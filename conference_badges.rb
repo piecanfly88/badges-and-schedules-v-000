e405b7acee8b6b15dac2926256f70e25496ad327
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-03 02:02:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,10 @@ def batch_badge_creator(names)
   names.each{|name| badges.push(badge_maker(name))}
   badges
 end
+
+def assign_rooms(names)
+  room_assignments = []
+  names.each_with_index do |name, index|
+  room_assignments.push("Hello, #{name}! You'll be assigned to room #{index}!"))
+  room_assignments
+end
